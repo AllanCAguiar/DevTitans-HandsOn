41823ef802f9d50b72e525ddfe85abecbba5211b
AllanCAguiar <allan.aguiar@icomp.ufam.edu.br>
2026-01-23 20:59:44 -0400

    First version of the new charger screen

--- pull_from_aosp.sh
+++ pull_from_aosp.sh
@@ -27,7 +27,7 @@ echo "ATENÇÃO: Copiando do AOSP ($AOSP_DIR) para REPO ($MODS_DIR)"
 echo "Apenas arquivos que JÁ EXISTEM no seu repositório serão atualizados."
 echo "-----------------------------------------------------"
 
-rsync -avh --progress --existing --update "${EXCLUDES[@]}" "$AOSP_DIR/" "$MODS_DIR/"
+rsync -avh --progress --existing "${EXCLUDES[@]}" "$AOSP_DIR/" "$MODS_DIR/"
 
 echo "Sincronização reversa concluída."
 
